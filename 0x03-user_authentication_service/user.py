81c846315b787989c12cf9bb32443abdcc2dcc00
MoazAhmedS <msayedmohamed@student.eelu.edu.eg>
2024-04-25 07:35:14 +0000

    taskdone

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module for the `User` model.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """Represents a user record in the database.
+
+    Attributes:
+        __tablename__ (str): The name of the table.
+        id (int): The unique identifier for the user.
+        email (str): The email address of the user.
+        hashed_password (str): The hashed password of the user.
+        session_id (str): The session ID of the user.
+        reset_token (str): The reset token of the user.
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
